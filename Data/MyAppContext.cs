8c28cd5d4a4abc0954c3722415d0824833b7169e
GricivRoman <griciv.roman.st@gmail.com>
2023-01-31 17:38:35 +0300

    deleted code comments

--- Data/MyAppContext.cs
+++ Data/MyAppContext.cs
@@ -13,51 +13,6 @@ namespace FoodDiary.Data
             
         }
 
-
-
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder);
-
-            //modelBuilder.Entity("FoodDiary.Data.Entities.Dish", b =>
-            //{
-            //    b.HasOne("FoodDiary.Data.Entities.ResourseSpecification", "ResourseSpecification")
-            //        .WithOne("Dish")
-            //        .HasForeignKey("FoodDiary.Data.Entities.Dish", "ResourseSpecificationId")
-            //        .OnDelete(DeleteBehavior.Cascade)
-            //        .IsRequired();
-
-            //    b.Navigation("ResourseSpecification");
-            //});
-
-
-
-            //modelBuilder.Entity("FoodDiary.Data.Entities.User", b =>
-            //{
-            //    b.HasOne("FoodDiary.Data.Entities.UserMenu", "UserMenu")
-            //        .WithOne("User")
-            //        .HasForeignKey("FoodDiary.Data.Entities.User", "UserMenuId")
-            //        .OnDelete(DeleteBehavior.Cascade)
-            //        .IsRequired();
-
-            //    b.Navigation("UserMenu");
-            //});
-
-            //modelBuilder.Entity("FoodDiary.Data.Entities.ResourseSpecification", b =>
-            //{
-            //    b.HasOne("FoodDiary.Data.Entities.DishValue", "DishValue")
-            //        .WithOne("ResourseSpecification")
-            //        .HasForeignKey("FoodDiary.Data.Entities.ResourseSpecification", "DishValueId")
-            //        .OnDelete(DeleteBehavior.Cascade)
-            //        .IsRequired();
-                    
-
-            //    b.Navigation("DishValue");
-            //});
-
-
-        //}
-
         public DbSet<User> User { get; set; }
         
         public DbSet<Product> Product { get; set; }
